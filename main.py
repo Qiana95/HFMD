63d8b3caf99b550886461fd6a1deae6c77a89e2d
juniorliu_pro <juniorliu95@gmail.com>
2022-09-19 16:46:08 +0900

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
 from scipy import stats
 
-
+# comment
 #%%
 excel = pd.read_excel("2016_lag_select.xlsx", sheet_name="lag_0")
 X = excel.drop(columns=["time", "cases"])
