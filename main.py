8c89f35e0df98de2f42c98eb1b60b5146ee1ea1a
牛茜 <gyuuakane@h36.248.242.10.1017426.vlan.kuins.net>
2022-09-19 16:54:24 +0900

    select

--- main.py
+++ main.py
@@ -12,11 +12,19 @@ excel = pd.read_excel("2016_lag_select.xlsx", sheet_name="lag_0")
 X = excel.drop(columns=["time", "cases"])
 y = excel.iloc[:,1]
 
-
-
 X2 = sm.add_constant(X)
 est = sm.OLS(y, X2)
 est2 = est.fit()
 print(est2.summary())
+
+#%%
+# pmax = -1
+# while True:
+#     if pmax != -1:
+#         X = X.drop(columns=)
+#     X2 = sm.add_constant(X)
+#     est = sm.OLS(y, X2)
+#     est2 = est.fit()
+#     print(est2.summary())
 
 # %%
